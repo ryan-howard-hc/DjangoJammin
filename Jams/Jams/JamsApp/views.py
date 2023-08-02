339b7ee271703aca7b46dfb5dcb955f44b494837
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-08-02 18:47:06 +0000

    fromdjango

--- Jams/Jams/JamsApp/views.py
+++ Jams/Jams/JamsApp/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from .models import Artist, Genre, Album, Song
+from .serializers import ArtistSerializer, GenreSerializer, AlbumSerializer, SongSerializer
